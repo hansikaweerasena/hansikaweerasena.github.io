ce2272aed84dc5236621297a26341ef80478c0a8
hanz94 <hansikam94@gmail.com>
2022-05-24 18:25:57 -0400

    Add recent highlights

--- src/App.tsx
+++ src/App.tsx
@@ -68,6 +68,33 @@ function App() {
           <div className="col"></div>
         </div>
       </div>
+
+      <div id="hanz-home-about-pre" className="container"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <h3>Recent Highlights</h3>
+          </div>
+          <div className="col">
+            <ul>
+              <li>Received Gartner Group Graduate Fellowship for outstanding CISE Ph.D. students by CISE department of
+                University of Florida - Apr 2022
+              </li>
+              <li>Our paper &quot;Lightweight Encryption Using Chaffing and Winnowing with All-or-Nothing Transform for
+                Network-on-Chip Architectures&quot; was accepted at <a
+                    href="https://ieeexplore.ieee.org/xpl/conhome/9702149/proceeding">2021 IEEE International Symposium
+                  on Hardware Oriented Security and Trust (HOST)</a></li>
+              <li>Received full assistantship for PhD program in Computer Science (CISE) from University of Florida -
+                Jan 2021
+              </li>
+              <li>Our paper &quot;Continuous Automatic Bioacoustics Monitoring of Bird Calls with Local Processing on
+                Node Level&quot; was accepted at <a href="https://ieeexplore.ieee.org/xpl/conhome/8643125/proceeding">TENCON
+                  2018 - 2018 IEEE Region 10 Conference</a>.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
