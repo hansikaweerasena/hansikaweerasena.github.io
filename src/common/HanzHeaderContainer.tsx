463b08971614e2c2a06b526bb6fc010ebdfb16b5
hanz94 <hansikam94@gmail.com>
2023-02-21 17:34:16 -0500

    Inbuilt show more button support in header container

--- src/common/HanzHeaderContainer.tsx
+++ src/common/HanzHeaderContainer.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 export function HanzHeaderContainer(props: any) {
     return <div className="container shadow-sm hanz-container">
+        <div className="row">
+            <div className="col-md-8">
                 <h4 className="fw-bold text-secondary hanz-ssection-header">{props.title}</h4>
-                {props.children}
+            </div>
+            {props.buttonText ?
+            <div className="col-md-4 text-md-end">
+                <button type="button" className="btn btn-link disabled">{props.buttonText}</button>
+            </div> : ''}
+        </div>
+        {props.children}
     </div>
 }
