d829da1bbdb74bcfb112ff82086e081c7616d7e6
hanz94 <hansikam94@gmail.com>
2023-02-21 17:53:38 -0500

    Css fixes

--- src/common/HanzHeaderContainer.tsx
+++ src/common/HanzHeaderContainer.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 export function HanzHeaderContainer(props: any) {
     return <div className="container shadow-sm hanz-container">
         <div className="row">
-            <div className="col-md-8">
+            <div className="col-8">
                 <h4 className="fw-bold text-secondary hanz-ssection-header">{props.title}</h4>
             </div>
             {props.buttonText ?
-            <div className="col-md-4 text-md-end">
+            <div className="col-4 text-end">
                 <button type="button" className="btn btn-link disabled">{props.buttonText}</button>
             </div> : ''}
         </div>
