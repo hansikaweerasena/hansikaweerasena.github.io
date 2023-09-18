279b7dd48cd55e7a7f4059933686db24d5117323
hanz94 <hansikam94@gmail.com>
2023-09-18 17:16:37 -0400

    Separate out home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
 import React from 'react';
 import './App.css';
 import {NavBar} from "./components/NavBar";
-import {HomeMain} from "./components/HomeMain";
-import {HomeRecent} from "./components/HomeRecent";
-import {HomePub} from "./components/HomePub";
-import {HomeEdu} from './components/HomeEdu';
 import {Footer} from "./components/Footer";
-import {HomeExp} from "./components/HomeExp";
-import {HomeAward} from "./components/HomeAward";
-import {HomeVol} from "./components/HomeVol";
-import {HanzSpaciousContainer} from "./common/HanzSpaciousContainer";
-import {HanzNarrowContainer} from "./common/HanzNarrowContainer";
-import {HomeAbout} from "./components/HomeAbout";
-import {HomeInsta} from "./components/HomeInsta";
+import {Home} from "./pages/Home"
+
 
 function App() {
     return (
         <div className="App">
             <NavBar></NavBar>
-            <div className="container">
-                <div className="row">
-                    <HanzNarrowContainer>
-                        <HomeMain></HomeMain>
-                        <HomeAbout></HomeAbout>
-                        <HomeRecent></HomeRecent>
-                        <HomeInsta></HomeInsta>
-                    </HanzNarrowContainer>
-                    <HanzSpaciousContainer>
-                        <HomeExp></HomeExp>
-                        <HomePub></HomePub>
-                        <HomeEdu></HomeEdu>
-                        <HomeAward></HomeAward>
-                        <HomeVol></HomeVol>
-                    </HanzSpaciousContainer>
-                </div>
-            </div>
+                <Home></Home>
             <Footer></Footer>
         </div>
     );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {HanzNarrowContainer} from "../common/HanzNarrowContainer";
+import {HomeMain} from "../components/HomeMain";
+import {HomeRecent} from "../components/HomeRecent";
+import {HomeInsta} from "../components/HomeInsta";
+import {HanzSpaciousContainer} from "../common/HanzSpaciousContainer";
+import {HomeExp} from "../components/HomeExp";
+import {HomePub} from "../components/HomePub";
+import {HomeEdu} from "../components/HomeEdu";
+import {HomeAward} from "../components/HomeAward";
+import {HomeVol} from "../components/HomeVol";
+import {HomeAbout} from "../components/HomeAbout";
+
+export function Home() {
+    return (<div className="container">
+            <div className="row">
+                <HanzNarrowContainer>
+                    <HomeMain></HomeMain>
+                    <HomeAbout></HomeAbout>
+                    <HomeRecent></HomeRecent>
+                    <HomeInsta></HomeInsta>
+                </HanzNarrowContainer>
+                <HanzSpaciousContainer>
+                    <HomeExp></HomeExp>
+                    <HomePub></HomePub>
+                    <HomeEdu></HomeEdu>
+                    <HomeAward></HomeAward>
+                    <HomeVol></HomeVol>
+                </HanzSpaciousContainer>
+            </div>
+        </div>
+);
+}
