b4257c6e9d76e15f702c6842c97ca59672588df0
hanz94 <hansikam94@gmail.com>
2023-11-01 17:46:56 -0400

    Add react routing for about page

--- src/components/HomeAbout.tsx
+++ src/components/HomeAbout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {HanzHeaderContainer} from "../common/HanzHeaderContainer";
 
 export function HomeAbout() {
-    return (<HanzHeaderContainer title="About Me" buttonText="Read More">
+    return (<HanzHeaderContainer title="About Me" buttonText="Read More" buttonPath="/about">
             <p id="hanz-home-about-p" className="fw-normal">Hansika is a PhD Candidate at the
                 Department
                 of
