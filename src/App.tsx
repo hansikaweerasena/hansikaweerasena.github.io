c6029942026f4b10baf4ba862fe8cee74f521ac5
hanz94 <hansikam94@gmail.com>
2023-11-01 17:40:02 -0400

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import './App.css';
 import {NavBar} from "./components/NavBar";
 import {Footer} from "./components/Footer";
 import {Home} from "./pages/Home"
+import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import {Experience} from "./pages/Experience";
+import {NotFound} from "./pages/NotFound";
+import {About} from "./pages/About";
+import {Publications} from "./pages/Publications";
 
 
 function App() {
     return (
         <div className="App">
             <NavBar></NavBar>
-                <Home></Home>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/experience" element={<Experience />} />
+                        <Route path="/publications" element={<Publications />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Router>
             <Footer></Footer>
         </div>
     );
