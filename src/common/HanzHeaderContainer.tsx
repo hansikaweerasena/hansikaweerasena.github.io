31bf81d21c2ecbad480c4b3d0e648c4d2f89f716
hanz94 <hansikam94@gmail.com>
2023-11-01 17:45:01 -0400

    Test header container with read more and expansion

--- src/common/HanzHeaderContainer.tsx
+++ src/common/HanzHeaderContainer.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function HanzHeaderContainer(props: any) {
+
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        if (props.buttonPath) {
+            navigate(props.buttonPath);
+        }
+    };
+
     return <div className="container shadow-sm hanz-container">
         <div className="row">
             <div className="col-8">
@@ -8,7 +18,7 @@ export function HanzHeaderContainer(props: any) {
             </div>
             {props.buttonText ?
             <div className="col-4 text-end">
-                <button type="button" className="btn btn-link disabled">{props.buttonText}</button>
+                <button type="button" className="btn btn-link" onClick={handleButtonClick}>{props.buttonText}</button>
             </div> : ''}
         </div>
         {props.children}
