e3a7686a3ad88587cbfbccd8a2df1017202bd90b
hansikaweerasena <hansikam94@gmail.com>
2024-08-28 15:54:48 -0400

    Add dynamic list and scroll bar to recent highlights

--- src/components/HomeRecent.tsx
+++ src/components/HomeRecent.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import {HanzHeaderContainer} from "../common/HanzHeaderContainer";
+import { HanzHeaderContainer } from "../common/HanzHeaderContainer";
+import data from '../data/recent.json';
 
 export function HomeRecent() {
+    const recentHighlights = data.recentHighlights;
+
+    // You can adjust the style here if you want a specific height different from Bootstrap defaults
+    const listStyle = {
+        maxHeight: '400px', // You can adjust this height as needed
+    };
+
     return (
         <HanzHeaderContainer title="Recent Highlights">
-                    <ul>
-                        <li>Our survey paper &quot;  Security of Electrical, Optical and Wireless On-Chip Interconnects: A Survey &quot; was accepted at <a
-                            href="https://dl.acm.org/doi/10.1145/3631117"> ACM Transactions on Design Automation of Electronic Systems (TODAES)</a> - Nov 2023
-                        </li>
-                        <li>Received Gartner Group Graduate Fellowship for outstanding CISE Ph.D. students by CISE department of
-                            University of Florida - Apr 2023
-                        </li>
-                        <li>Our short paper &quot; Preserving Confidentiality and Anonymity in NoC-based SoCs &quot; was accepted at <a
-                            href="https://www.gomactech.net/"> GOMACTech-23</a> for a poster presentation - Nov 2022
-                        </li>
-                        <li>Received Gartner Group Graduate Fellowship for outstanding CISE Ph.D. students by CISE department of
-                            University of Florida - Apr 2022
-                        </li>
-                        <li>Our paper &quot;Lightweight Encryption Using Chaffing and Winnowing with All-or-Nothing Transform for
-                            Network-on-Chip Architectures&quot; was accepted at <a
-                                href="https://ieeexplore.ieee.org/xpl/conhome/9702149/proceeding">2021 IEEE International Symposium
-                                on Hardware Oriented Security and Trust (HOST)</a></li>
-                        <li>Received full assistantship for PhD program in Computer Science (CISE) from University of Florida -
-                            Jan 2021
-                        </li>
-                        <li>Our paper &quot;Continuous Automatic Bioacoustics Monitoring of Bird Calls with Local Processing on
-                            Node Level&quot; was accepted at <a href="https://ieeexplore.ieee.org/xpl/conhome/8643125/proceeding">TENCON
-                                2018 - 2018 IEEE Region 10 Conference</a>.
-                        </li>
-                    </ul>
+            <ul className="overflow-auto" style={listStyle}>
+                {recentHighlights.map((highlight, index) => (
+                    <li key={index}>
+                        {highlight.descriptions.map((desc, descIndex) => (
+                            <span key={descIndex}>
+                                {desc.preText}
+                                {desc.link && <a href={desc.link}>{desc.linkText}</a>}
+                                {desc.postText}
+                            </span>
+                        ))}
+                    </li>
+                ))}
+            </ul>
         </HanzHeaderContainer>
     );
 }
