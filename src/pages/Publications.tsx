40b534a968cb5d956cc73e2a89fd847dead21d07
hansikaweerasena <hansikam94@gmail.com>
2025-05-19 16:39:13 -0400

    Add research overview to publications

--- src/pages/Publications.tsx
+++ src/pages/Publications.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { HanzFullWidthContainer } from "../common/HanzFullWidthContainer";
 import publicationsData from '../data/publications.json';
-import {HanzCardArticleContainer} from "../common/HanzCardPubContainer"; // Update the path as necessary
+import {HanzCardArticleContainer} from "../common/HanzCardPubContainer";
+import {useTheme} from "../ThemeContext"; // Update the path as necessary
 
 export function Publications() {
 
@@ -9,11 +10,36 @@ export function Publications() {
         return content.length > wordLimit ? `${content.substring(0, wordLimit)}...` : content;
     };
 
+    const { theme } = useTheme();
+
+    const imgSrc = theme === 'dark' ? 'img/article/overview_d.png' : 'img/article/overview_l.png';
+
 
     return (
         <div className="container">
             <div className="row">
                 <HanzFullWidthContainer>
+                    <div className="col">
+                        <div className="row">
+                            <h1>Research Overview</h1>
+                            <div className="container shadow-sm hanz-container">
+                                <div className="row">
+                                    <div style={{width: '100%', height: '100%'}}>
+                                        <img className="hanz-img-responsive" src={imgSrc} alt={"props.title"}/>
+                                    </div>
+                                    <div style={{marginTop: '1rem'}}/>
+                                    <hr/>
+                                    <p className="fw-normal">
+                                        My research interests lie in computer architecture, computer and network
+                                        security, and applied machine learning for security. During my Ph.D., I have
+                                        worked on network-on-chip security, confidential computing (trusted execution
+                                        environments), and security verification.
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                     <div className="col">
                         <h1>Journal</h1>
                         <hr/>
@@ -53,10 +79,10 @@ export function Publications() {
                             ))}
                         </div>
                     </div>
-                        <div className="col">
-                            <h1>Preprints</h1>
-                            <hr/>
-                            <div className="row">
+                    <div className="col">
+                        <h1>Preprints</h1>
+                        <hr/>
+                        <div className="row">
                             {publicationsData.preprints.map((pre: any, index: React.Key) => (
                                 <HanzCardArticleContainer
                                     key={`preprint-${index}`}
@@ -72,7 +98,7 @@ export function Publications() {
                                 />
                             ))}
                         </div>
-                        </div>
+                    </div>
                 </HanzFullWidthContainer>
             </div>
         </div>
